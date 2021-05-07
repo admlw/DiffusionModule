ea0a29329a0ffa65878be1ce6943a55d3c6bd61a
Andrew Mogan <amogan@vols.utk.edu>
2021-05-07 15:22:13 -0500

    Reverted FindMaximumValue function in Utilities.cxx to use the median, as opposed to Michelle's method

--- Algorithms/Utilities.cxx
+++ Algorithms/Utilities.cxx
@@ -42,7 +42,7 @@ namespace diffmod {
     return returnerThetaYZ;
   }
 
-  /*
+  // Find median hit width value in bin of drift time
   double Utilities::FindMaximumValue(TH1D* h){
   
     double maxBinValue = h->GetBinCenter((h->GetMaximumBin()));
@@ -58,102 +58,113 @@ namespace diffmod {
     return value;
 
   }
-  */
 
   // Michelle's version
+  // This was only used for some tests and isn't actually used in the paper version
+  // Uncommenting this may cause some build errors
+  /*
   double Utilities::FindMaximumValue(TH1D* h){
 
-    float qq[25]; float eq[25];
-    float   dt[25],edt[25];
-    float   kk[25],ek[25];
+    //float   qq[25],eq[25];
+    //float   dt[25],edt[25];
+    //float   kk[25],ek[25];
 
     int iwlow = 0; int iwhigh=0;
-    int npts = 0;
-    TH1F dq[25];  TH1F dtime[25];
+    //int npts = 0;
+    double fit_max = -999; // Return val
+    //TH1F dq[25];  
+    TH1F dtime[25];
 
-    gStyle->SetOptFit(1111);
+    //gStyle->SetOptFit(1111);
     //TCanvas* c1 = new TCanvas("c1");
 
-      //TString label = Form("DiffusionModule/plane%1d/h_sigma_%1d_plane%1d",iplane,i,iplane);
-      //dq[i] = *((TH1F*)f->Get(label)); 
-      dq[i] = h; 
-      //std::cout << dq[i].GetEntries() << std::endl;
-      //  Could be 50000  . . . see if uncertainty reflects this
-      if (dq[i].GetEntries()>2000) {
-        int mbin = dq[i].GetMaximumBin();
-        float xmid = dq[i].GetBinCenter(mbin);
-        float maxh=dq[i].GetBinContent(mbin);
-        // dynamic width
-        float target=0.5*maxh;
-        int thisbin=mbin-1;  float thish=dq[i].GetBinContent(thisbin);
-        while (thish>target) {
-          thisbin--;
-          thish=dq[i].GetBinContent(thisbin);
-        }
-        iwlow = thisbin;
-        std::cout << "[MAXFIT] lowedge " << iwlow << " center " << mbin << std::endl;
-        thisbin=mbin+1;  thish=dq[i].GetBinContent(thisbin);
-        while (thish>target) {
-          thisbin++;
-          thish=dq[i].GetBinContent(thisbin);
-        }
-        iwhigh = thisbin;
-        std::cout << "[MAXFIT] highedge " << iwhigh << " center " << mbin << std::endl;
-        /// end dynamic width
-        float lowedge = dq[i].GetBinCenter(iwlow);
-        float highedge = dq[i].GetBinCenter(iwhigh);
-        TF1 *f1 = new TF1("f1", "gaus",lowedge,highedge);
-        f1->SetParameter(0,xmid);
-        f1->SetParameter(1,xmid);
-        float temp = xmid-lowedge;
-        f1->SetParameter(2,temp);
-        dq[i].Fit("f1","R");
-        qq[npts] = f1->GetParameter(1);
-        eq[npts] = f1->GetParError(1);
-        dt[npts]=i; edt[npts]=0;
-        kk[npts] = f1->GetParameter(2);
-        ek[npts] =f1->GetParError(2);
-        //      edt[npts]= 69./2/1.414;  // uncertainty = full width of a strip in drift time divided by 2*sqrt(2)
-        npts++;
+    //TString label = Form("DiffusionModule/plane%1d/h_sigma_%1d_plane%1d",iplane,i,iplane);
+    //h = *((TH1F*)f->Get(label)); 
+    //std::cout << h->GetEntries() << std::endl;
+    //  Could be 50000  . . . see if uncertainty reflects this
+    if (h->GetEntries()>2000) {
+
+      int   mbin = h->GetMaximumBin();
+      float xmid = h->GetBinCenter(mbin);
+      float maxh=h->GetBinContent(mbin);
+
+      // dynamic width
+      float target=0.7*maxh; // Was 0.5
+      int thisbin=mbin-1;  float thish=h->GetBinContent(thisbin);
+
+      while (thish>target) {
+        thisbin--;
+        thish=h->GetBinContent(thisbin);
       }
-      else {
-        // qq[i]=1500.0;0
-        // eq[i]=1500.0;
-        // dt[i]=(i+0.5)*1250.0/18.;
-        // edt[i]=1000.0;
+      iwlow = thisbin;
+      std::cout << "[MAXFIT] lowedge " << iwlow << " center " << mbin << std::endl;
+      thisbin=mbin+1;  thish=h->GetBinContent(thisbin);
+
+      while (thish>target) {
+        thisbin++;
+        thish=h->GetBinContent(thisbin);
       }
-      
-      TString name = outdir+Form("fit_plane%1d_bin%2d",iplane,i)+".png";
-      c1->Print(name);
-      c1->Update();
-    //    std::cout << "dq " << i << " " << qq[i] << " " << dt[i] << std::endl;
+      iwhigh = thisbin;
+      std::cout << "[MAXFIT] highedge " << iwhigh << " center " << mbin << std::endl;
 
+      // end dynamic width
+      float lowedge  = h->GetBinCenter(iwlow);
+      float highedge = h->GetBinCenter(iwhigh);
 
-      std::cout << "npts " << npts << std::endl;
-      TCanvas* c2 = new TCanvas("c2");
-      TGraphErrors *gr = new TGraphErrors(npts,dt,qq,edt,eq);
-      gr->SetMarkerColor(4);
-      gr->SetMarkerStyle(21);
-      gr->GetXaxis()->SetTitle("drift time bin");
-      gr->GetYaxis()->SetTitle("peak position");
-      gr->Draw("AP");
-      c2->Print(outdir+Form("peak_plane%1d",iplane)+".png");
-      c2->Update();
-      
+      TF1 *f1 = new TF1("f1", "gaus",lowedge,highedge);
+      f1->SetParameter(0,xmid);
+      f1->SetParameter(1,xmid);
+      float temp = xmid-lowedge;
+      f1->SetParameter(2,temp);
+      h->Fit("f1","qR");
 
-      TGraphErrors *gr2 = new TGraphErrors(npts,dt,kk,edt,ek);
-      gr2->SetMarkerColor(4);
-      gr2->SetMarkerStyle(21);
-      gr2->GetXaxis()->SetTitle("drift time bin");
-      gr2->GetYaxis()->SetTitle("fit width");
-      gr2->Draw("AP");
+      fit_max = f1->GetParameter(1);
 
-      c2->Print(outdir+Form("width_plane%1d",iplane)+".png");
-      c2->Update();
-      
+      qq[npts] = f1->GetParameter(1);
+      eq[npts] = f1->GetParError(1);
+      dt[npts]=i; edt[npts]=0;
+      kk[npts] = f1->GetParameter(2);
+      ek[npts] =f1->GetParError(2);
+      //      edt[npts]= 69./2/1.414;  // uncertainty = full width of a strip in drift time divided by 2*sqrt(2)
+      npts++;
+    }
+    else {
+      std::cout << "[MAXFIT] Sigma dist has too few entries" << std::endl;
+    }
+    
+    TString name = outdir+Form("fit_plane%1d_bin%2d",iplane,i)+".png";
+    c1->Print(name);
+    c1->Update();
+  //    std::cout << "dq " << i << " " << qq[i] << " " << dt[i] << std::endl;
 
-      f->Close();
+    std::cout << "npts " << npts << std::endl;
+    TCanvas* c2 = new TCanvas("c2");
+    TGraphErrors *gr = new TGraphErrors(npts,dt,qq,edt,eq);
+    gr->SetMarkerColor(4);
+    gr->SetMarkerStyle(21);
+    gr->GetXaxis()->SetTitle("drift time bin");
+    gr->GetYaxis()->SetTitle("peak position");
+    gr->Draw("AP");
+    c2->Print(outdir+Form("peak_plane%1d",iplane)+".png");
+    c2->Update();
+    
+
+    TGraphErrors *gr2 = new TGraphErrors(npts,dt,kk,edt,ek);
+    gr2->SetMarkerColor(4);
+    gr2->SetMarkerStyle(21);
+    gr2->GetXaxis()->SetTitle("drift time bin");
+    gr2->GetYaxis()->SetTitle("fit width");
+    gr2->Draw("AP");
+
+    c2->Print(outdir+Form("width_plane%1d",iplane)+".png");
+    c2->Update();
+    
+
+    f->Close();
+
+    return fit_max;
 
   }
+  */
 
 }
